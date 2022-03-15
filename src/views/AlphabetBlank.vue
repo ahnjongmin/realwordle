20432360f5a0bdeae1360d78125c9d501f0a9f09
JongminAhn <ajm6962@hanmail.net>
2022-03-16 00:59:28 +0900

    chore: add sass-loader in package

---
+++ src/views/AlphabetBlank.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="blank-container">
+    <div class="blank">a</div>
+    <div class="blank">b</div>
+    <div class="blank">c</div>
+    <div class="blank">d</div>
+    <div class="blank">e</div>
+  </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+<style lang="scss" scoped>
+
+.blank-container {
+  width: 300px;
+  height: 60px;
+  background-color: blue;
+  display: flex;
+  
+  .blank {
+    height: 45px;
+    width: 45px;
+    border: 2px solid black;
+  }
+}
+
+</style>
--- src/views/WordleFive.vue
+++ src/views/WordleFive.vue
@@ -1,12 +1,17 @@
 <template>
   <button @click="addCount">{{count}}</button>
+  <AlphabetBlank />
 </template>
 
 <script>
 // 컴포넌트, props을 제외한 모든 것들이 전부 setup 안에 들어감
 import { ref, reactive, computed, onMounted } from 'vue'
 import WordleManager from '../core/wordle-manager.js'
+import AlphabetBlank from './AlphabetBlank.vue'
 export default {
+    components: {
+        AlphabetBlank,
+    },
     setup() {
         const count = ref(0);
         const reactiveA = reactive({});
